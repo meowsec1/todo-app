7c95e2afde2a5891be2382bd9d7ac8fadf6dade4
v <128931588+meowsec1@users.noreply.github.com>
2025-09-13 03:32:01 +0700

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 console.log("Hello world!");
+
+function toDoItemFactory(title, description, dueDate, priority) {
+    let isComplete = false;
+    return {
+        title,
+        description,
+        dueDate,
+        priority,
+        isComplete,
+        markComplete: () => !isComplete
+    }
+}
+
+function projectFactory(title) {
+    const toDos = [];
+    return {
+        title,
+        deleteToDo(indexToRemove) {
+            if (indexToRemove >= 0 && indexToRemove < toDos.length) {
+                toDos.splice(indexToRemove, 1);
+            }
+        },
+        addToDo(toDo) {
+            toDos.push(toDo);
+        },
+        get toDos() {
+            return [...toDos];
+        }
+    }
+}
+
+const item = toDoItemFactory("walk dog", "walk the dog today", "tomorrow", "high");
+const schoolWork = projectFactory("School Work");
+const realWork = projectFactory("Real Work");
+
+realWork.addToDo(item);
+
+console.log(realWork.toDos);
+console.log(schoolWork.toDos);
+
+
+console.log(item.markComplete());
